6c6221ee41b0aedd76f8d564b802451c2dcbcbb3
Aisha Patro <aishapatra365@gmail.com>
2026-08-07 11:30:13 +0530

    Add files via upload

---
+++ Variables.py
@@ -0,0 +1,40 @@
+
+# Variables in python
+first_name = 'AISHARANI'
+last_name ='PATRO'
+country = 'HYD'
+city ='TELEGANA'
+age = 40087
+is_married = None
+skills = ['c','c++','python','oops']
+person_info={
+    'firstname':'aisharani',
+    'lastname':'Patro',
+    'country':'HYD',
+    'city':'TELEGANA'
+}
+
+ # Printing the values stored in the variables
+ 
+print('First name:', first_name)
+print('First name length:', len(first_name))
+print('Last name:', last_name)
+print('Last name length:', len(last_name))
+print('Country:', country)
+print('City:', city)
+print('Age:',age)
+print('Unmarried:', is_married)
+print('Skills:', skills)
+print('Person information:', person_info)
+ 
+ #Declearin multiple variables in one line
+first_name,last_name,country,age,is_unmarried = 'aisharani','patro','HYD',250,None
+ 
+print(first_name,last_name,country,age,is_unmarried)
+print('First name:', first_name)
+print('Last name:', last_name)
+print('Country:', country)
+print('Age:',age)
+print('Unmarried:',is_unmarried)
+  
+ 
